c39ba639f2c80350acad1e0165bbfe5becc4e204
Emmanuel Sasu <nanabingies1@gmail.com>
2016-06-07 00:30:21 +0000

    Create list_directory.c
    file to list the files in a directory

---
+++ list_directory.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+int main(int argc, char* argv[]){
+    DIR* dir;
+    struct dirent dirptr;
+    if (argc < 2){
+        printf("Usage: ./name_of_of <directory_name>\n");
+        return EXIT_FAILURE;
+    }
+    dir = opendir(argv[1]);
+    if (!dir){
+        printf("Can't open %s directory\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+    while ((dirptr=readdir(dir)) != NULL){
+        printf("%s\n", dirptr->d_name);
+    }
+    return EXIT_SUCCESS;
+}
