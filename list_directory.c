35726828c2e00c78a582728672e7b6f3335af315
Emmanuel Sasu <nanabingies1@gmail.com>
2016-06-22 00:39:00 +0000

    Update list_directory.c

--- list_directory.c
+++ list_directory.c
@@ -1,6 +1,8 @@
+/* This is a c file to list all files and folders in a directory */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
+#include <dirent.h>     /* dir header file for posix */
 int main(int argc, char* argv[]){
     DIR* dir;
     struct dirent *dirptr;
@@ -8,13 +10,13 @@ int main(int argc, char* argv[]){
         printf("Usage: ./name_of_of <directory_name>\n");
         return EXIT_FAILURE;
     }
-    dir = opendir(argv[1]);
+    dir = opendir(argv[1]);     /* opens directory if it exists */
     if (!dir){
         printf("Can't open %s directory\n", argv[1]);
-        return EXIT_FAILURE;
+        return EXIT_FAILURE;            /* exit if directory does not exist */
     }
-    while ((dirptr=readdir(dir)) != NULL){
-        printf("%s\n", dirptr->d_name);
+    while ((dirptr=readdir(dir)) != NULL){      /* read dir */
+        printf("%s\n", dirptr->d_name);         /* write contents to stdout */
     }
     return EXIT_SUCCESS;
 }
